0c1155e37a6f9e9ce8c2e785c717ab7bd35e8f46
Sai <saisumedh16@gmail.com>
2021-12-15 01:36:23 +0530

    article-card update

--- CardComponent/Card/index.js
+++ CardComponent/Card/index.js
@@ -1,20 +1,55 @@
 import prompt from '@system.prompt';
+import articleComp from './article.js';
+
 export default {
     data: {
+        container: "",
+        datetimevalue:"",
+        imageClass: '',
+        title: "",
+        subTitle: "",
+        textContainer: "",
+        bottomContainer: "",
+        pickerdatetime: "",
+        dateValue: "",
+        description: "",
+    },
+    props:{
         type: "",
-        datetimevalue:'',
-        datetimeselect:'2021-12-14-12-00',
+        name:"",
+        sub_title: "",
+        image: "",
+        desc:"",
     },
     onInit() {
         if(this.type==="appointment-card"){
-            this.type="container";
+            this.container="appointment-card";
+            this.imageClass="image";
+            this.title="name";
+            this.subTitle= "spec";
+            this.textContainer="textContainer";
+            this.bottomContainer="bottomContainer";
+            this.pickerdatetime="pickerdatetime";
+            this.dateValue="text";
         }
-        else if(this.type==="card"){
-            this.type="card";
+//        else if(this.type==="card"){
+//            this.container="card";
+//        }
+        else if(this.type==="article"){
+            this.container="article";
+            this.imageClass="article_image";
+            this.title="title";
+            this.subTitle= "subTitle";
+            this.textContainer="articleTextContainer";
+            this.bottomContainer="bottomBlock";
+            this.pickerdatetime="pickerBlock";
+            this.dateValue="dateBlock";
+            this.description="description";
         }
     },
     datetimeonchange(e) {
         const zeroPad = (num, places) => String(num).padStart(places, '0');
+        e.month=e.month+1;
         this.datetimevalue=e.day+"/"+e.month+"/"+e.year+"  "+zeroPad(e.hour,2)+":"+zeroPad(e.minute,2);
     },
     datetimeoncancel() {
